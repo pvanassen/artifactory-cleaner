f189126479c4001145dbfb96cf8662a5ba99ddde
Paul van Assen <source@paules.nl>
2013-12-23 16:24:49 +0100

    Initial project setup

---
+++ src/main/java/nl/pvanassen/artifactory/cleaner/Main.java
@@ -0,0 +1,18 @@
+package nl.pvanassen.artifactory.cleaner;
+
+/**
+ * Entry point for the cleaner
+ * @author Paul van Assen
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
